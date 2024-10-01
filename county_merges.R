8b1ed65928f91a3cfa6a00deba09185fceb3da53
Greg Hamlin <greg.r.hamlin@gmail.com>
2024-10-01 10:53:31 -0400

    County merge update

---
+++ county_merges.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+
+county_2022_oc2 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2022_oc2.csv')
+county_2022_oc1 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2022_oc1.csv')
+county_2022 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2022.csv')
+county_2021_oc2 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2021_oc2.csv')
+county_2021_oc1 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2021_oc1.csv')
+county_2021 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2021.csv')
+county_2020 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2020.csv')
+county_2019 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2019.csv')
+county_2018 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2018.csv')
+county_2017 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2017.csv')
+county_2016 <- read_csv('County_level_Aggregate_Expenditure_and_Risk_Score_Data_on_Assignable_Beneficiaries_2016.csv')
